cac1b717324588637720dcffe386bf2cfdf5b1eb
Larieux <larieux.professionnelle@gmail.com>
2026-04-04 02:23:33 +0200

    add 04: 00 - 07

--- exercises/04_traits/05_trait_bounds/src/lib.rs
+++ exercises/04_traits/05_trait_bounds/src/lib.rs
@@ -6,8 +6,10 @@
 // collections (e.g. BTreeMap).
 
 /// Return the minimum of two values.
-pub fn min<T>(left: T, right: T) -> T {
-    if left <= right {
+pub fn min<T>(left: T, right: T) -> T
+where T: std::fmt::Debug + std::cmp::PartialOrd
+{
+	if left <= right {
         left
     } else {
         right
